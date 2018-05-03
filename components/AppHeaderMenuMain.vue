94bc78625b2e128a96400b2f8e44376c8ea689a7
aivat <aivat@mail.ru>
2018-05-03 17:57:44 +0500

    Deploy

---
+++ components/AppHeaderMenuMain.vue
@@ -0,0 +1,221 @@
+<template>
+  <header class="header-top sticky header">
+      <div class="container">
+          <div class="header-top">
+            <div class="header-left">
+                <a  href="tel: 608-464" class="href-contact">
+                    <img src="~static/logo.png" alt="Логотип 4 ПРАВИЛА"  class="logo-img">
+                </a>
+            </div>
+            <div class="header-right">
+                <a  href="tel: 608-464" class="href-contact">
+                    <svg class="iconic" fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
+                    </svg>
+                    93-50-60
+                </a>
+            </div>
+
+            <div class="header-right">
+                <span  class="href-contact menu-svg">
+                    <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
+                    </svg>
+                    <ul class="submenu">
+                        <li>
+                            <a href="#">О НАС</a>
+                        </li>
+                        <li>
+                            <a href="#">БЛОГ</a>
+                        </li>
+                        <li>
+                            <a href="#">КАК МЫ РАБОТАЕМ</a>
+                        </li>
+                        <li>
+                            <a href="#">КОНТАКТЫ</a>
+                        </li>
+                        <li>
+                            <a href="#">ЦЕНЫ</a>
+                        </li>    
+                    </ul>
+                </span>
+
+            </div>
+
+        </div>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+    data () {
+      return {
+        online: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+.header {
+    box-shadow: 0 -1px 8px rgba(0,0,0,.3);
+    padding: 5px 0;
+}
+.container {
+    width: 100%; 
+}
+.header-top {
+    display: flex;
+    justify-content: space-between;
+    background-color: white;
+    z-index: 9999;
+}
+
+.header-left, .header-right {
+   display: flex;
+   padding: 10px 10px;
+}
+.header-left {
+    padding-right: 0;
+}
+
+.header-right {
+    padding-left: 0;
+}
+.sticky {
+    position: -webkit-sticky;
+    position: -moz-sticky;
+    position: -ms-sticky;
+    position: -o-sticky;
+    position: sticky;
+    top: 0;
+    padding: 0;
+}
+
+.href-contact {
+	text-decoration: none;
+	/* border-bottom: 2px dashed #000000; */
+    color: black;
+    color: #676767;
+    font-weight: 500;
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+}
+
+.href-contact:hover {
+    color: #f00000;
+ }
+
+.href-contact-adress {
+    width: 215px;
+}
+
+.iconic-adress {
+    width: 34px;
+    height: 34px;
+}
+.iconic {
+    margin-right: 5px;
+    fill: currentColor;
+}
+.logo-img {
+    height: 40px;
+}
+li {
+    list-style-type: none; /* Убираем маркеры */
+}
+ul {
+    margin-left: 0; /* Отступ слева в браузере IE и Opera */
+    padding-left: 0; /* Отступ слева в браузере Firefox, Safari, Chrome */
+}
+.submenu {
+    /* display: none; */
+    display: block;
+    position: absolute;
+    padding: 0;
+    /* border: 2px solid white; */
+    border-radius: 2px;
+
+    background-color: rgba(0, 0, 0, 0.644);
+    background-color: white;
+     top: 70px;
+    right: 10px;
+    padding: 10px 0;
+    max-height: 0px;
+    /* height: 0px;
+    width: 0px; */
+    max-width: 0px;
+    transform: translateZ(0);
+background-color: white;
+color:#676767;
+  
+  /* transition: background-color 1s cubic-bezier(0.1, 0.8, 0.5, 2), color 0.2s linear; */
+}
+
+
+.submenu>li>a {
+    display: block;
+    padding: 5px 20px 5px 40px;
+    color: black;
+    color: #676767;
+    color: inherit;
+    width: 100%;
+    text-decoration: none;
+}
+
+.submenu>li>a:hover {
+    background-color: rgba(0, 0, 0, 0.1)
+}
+.menu-svg {
+    cursor: pointer;
+    /* display: inline-block; */
+}
+.menu-svg:hover>.submenu {
+     transition: right .2s cubic-bezier(0.0,0.0,0.2,1),max-width .2s cubic-bezier(0.0,0.0,0.2,1),max-height .2s cubic-bezier(0.0,0.0,0.2,1),opacity .05s linear,top .2s cubic-bezier(0.0,0.0,0.2,1); 
+        box-shadow: 0 8px 10px 1px rgba(0,0,0,0.14), 
+    0 3px 14px 2px rgba(0,0,0,0.12), 
+    0 5px 5px -3px rgba(0,0,0,0.2);
+    display: block;
+    max-height: 300px;
+    /* height: 300px; */
+     max-width: 400px;
+
+}
+@media (min-width: 360px) {
+    .href-contact-adress {
+        width: 235px;
+    }
+
+    .href-contact {
+        font-size: 14px;
+    }
+}
+
+@media (min-width: 500px) {
+    .href-contact-adress {
+        width: auto;
+    }
+    .iconic-adress {
+        width: 24px;
+        height: 24px;
+    }
+}
+
+@media (min-width: 1200px) {
+    .header {
+        justify-content: center;
+    }
+    .container {
+        width: 1200px; 
+    }
+    .href-contact {
+        font-size: 16px;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <AppHeaderMenuMain/>
+  </div>
+</template>
+
+<script>
+import AppHeaderMenuMain from '~/components/AppHeaderMenuMain.vue'
+
+export default {
+  components: {
+    AppHeaderMenuMain
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700');
+body {
+  font-family: roboto;
+  font-size: 16px;
+}
+</style>
